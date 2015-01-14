1d44b1a0d4ad0f399d462ed51987dc51387cc8fa
lijiabin <superleexpert@gmail.com>
2015-01-14 14:50:49 +0800

    update dalegate

--- ZLTableViewTest/ZLTableViewTest/ViewController.swift
+++ ZLTableViewTest/ZLTableViewTest/ViewController.swift
@@ -23,7 +23,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         dataArray.addObject("data5")
         dataArray.addObject("data6")
         
-        self.testTableView = UITableView(frame: self.view.bounds)
+        self.testTableView = UITableView(frame: self.view.bounds)//MD够简洁~
         /*调用方法、属性、下标索引等前面需要加上一个?，如果是nil值，也就是Optional.None，会跳过后面的操作不执行，如果有值，就是Optional.Some，可能就会拆包(unwrap)，然后对拆包后的值执行后面的操作，来保证执行这个操作的安全性*/
         self.testTableView?.delegate = self
         self.testTableView?.dataSource = self
@@ -42,15 +42,18 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("Cell") as UITableViewCell //as是类型转换，可理解为把什么当成什么的意思
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell") as UITableViewCell //as是类型转换，可理解为把什么当成什么的意思，这里Swift的用法和OC中的UICollectionView相似，初始化时调用registerClass
         cell.textLabel.text = String(format: "%d", indexPath.row)
         cell.contentView.backgroundColor = UIColor.blueColor()
         
         return cell
     }
     
-    func tableView(tableView: UITableView, didDeselectRowAtIndexPath indexPath: NSIndexPath) {
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         println("fuck the table view index row: \(indexPath.row+1)")
+        var alertView: UIAlertView
+        alertView = UIAlertView(title: "Got index", message: String(format: "Row: %d", indexPath.row+1), delegate: nil, cancelButtonTitle: "Back")
+        alertView.show()
     }
 }
 
